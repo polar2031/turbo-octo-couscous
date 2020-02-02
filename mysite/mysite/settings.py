4ac64914fe25536e4ba9a1f5b8caf40d99ce84b7
polar2031 <polar2031@gmail.com>
2020-02-03 02:06:15 +0800

    fix: heroku static file setting

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -118,6 +118,19 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
 
 URL_LENGTH = 6
+
+# Setting for heroku static file
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
